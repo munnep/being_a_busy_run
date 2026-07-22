c7ab41fa7fad3c5b4eb5e91a946f954afab6d8f1
munnep <patrick.munne@hashicorp.com>
2026-07-22 15:42:25 +0200

    some cpu usage

--- main.tf
+++ main.tf
@@ -6,10 +6,28 @@ EOT
   ]
 }
 
+data "external" "cpu_burn" {
+  program = ["bash", "-c", <<EOT
+    # Burn CPU for ~30 seconds using arithmetic in a tight loop
+    end=$(( $(date +%s) + 30 ))
+    count=0
+    while [ $(date +%s) -lt $end ]; do
+      count=$(( count + 1 ))
+      x=$(( count * count * 31337 ))
+    done
+    echo "{ \"result\": \"burned $count cycles\" }"
+EOT
+  ]
+}
+
 output "delay_result" {
   value = data.external.slow_delay.result
 }
 
+output "cpu_burn_result" {
+  value = data.external.cpu_burn.result
+}
+
 resource "null_resource" "name" {
-  
+
 }
